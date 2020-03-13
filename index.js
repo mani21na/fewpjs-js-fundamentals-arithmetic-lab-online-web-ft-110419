c23fd7e60fb27056223ee6f1c96c1c3730f3d330
Hanna Lee <mani21na@gmail.com>
2020-03-13 03:31:22 +0000

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 // Write your code here
 const num1 = 2;
 const num2 = 31;
-let multiply = num1 * num2;
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const mod = 10 % 6;
+
+const max = Math.max(1, 2, 3, 4, 20);
